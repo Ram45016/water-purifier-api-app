20a3785790df9705a724ee589722c0589c092b94
Ram45016 <ramu1652004@gmail.com>
2025-08-16 15:50:23 +0530

    changed named export

--- db.js
+++ db.js
@@ -1,13 +1,18 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pg;
+
 const connectionString = process.env.DATABASE_URL;
 if (!connectionString) {
-  console.warn('DATABASE_URL missing in env');
+  console.warn("DATABASE_URL missing in env");
 }
 
 const pool = new Pool({
   connectionString,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
 });
 
-module.exports = pool;
+export default pool;
